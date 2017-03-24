0663bfae709cf2009fa5a4fe499a8bf811bea816
Pujan Parikh <Pujan Parikh>
2017-03-24 14:12:59 -0700

    final release

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,43 @@
 var app = angular.module('chatApp', ['ui.bootstrap', 'firebase']);
 
+var onblur;
+var onblur2;
 
+/*app.controller('ChatController', function($scope, $firebaseArray) {
 
+    window.onblur = function() { onblur = true; console.log("onblur room 1"); };
+    window.onfocus  = function() { onblur = false;  console.log("onfocus room 1"); };
 
-app.controller('ChatController', function($scope, $firebaseArray) {
     var ref = firebase.database().ref().child('messages').limitToLast(5);
     $scope.messages = $firebaseArray(ref);
-    var onblur;
+    
+    
     firebase.database().ref().child('messages').on('value', function(snapshot){
 
-    	console.log("updated one room");
+        console.log("goes inside update 1");
+
+        window.onblur = function() { onblur2 = true; console.log("onblur room 2"); };
+        window.onfocus  = function() {onblur2 = false;  console.log("onfocus room 2"); };
+    	
           if (Notification.permission !== "granted")
                     Notification.requestPermission();
-                  else {
+          else {
                     
-                    window.onblur = function() { onblur = true; console.log("onblur"); };
-                    window.onfocus  = function() {onblur = false;  console.log("onfocus"); };
-                    if(onblur) {
+               
+                    
+                if(onblur) {
                     var notification = new Notification('Anguchat', {
-                      icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
-                      body: "New Message from room 1",
-                    });
+                          icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+                           body: "New Message from room 1",
+                     });
 
                     notification.onclick = function () {
-                      window.focus(); 
+                       window.focus(); 
                        $('#onebutton').trigger('click');
                     };
-                   }
-
-                  }
+                }
+            }
     });
-
-
 
      $scope.send = function(message) {
         $scope.messages.$add({
@@ -39,25 +45,76 @@ app.controller('ChatController', function($scope, $firebaseArray) {
             date: Date.now()
         })
     }
+}) */
 
-})
 
-app.controller('ChatController2', function($scope, $firebaseArray) {
-		    var ref2 = firebase.database().ref().child('messages2').limitToLast(5);
-		    $scope.messages2 = $firebaseArray(ref2);
-            var onblur2
-		    firebase.database().ref().child('messages2').on('value', function(snapshot){
+app.controller('ChatController', function($scope, $firebaseArray) {
 
-            console.log("goes inside permission 2");
+            window.onblur = function() { onblur = true; console.log("onblur room 1"); };
+            window.onfocus  = function() {onblur = false;  console.log("onfocus room 1"); };
 
-           
+            var ref = firebase.database().ref().child('messages').limitToLast(5);
+            $scope.messages = $firebaseArray(ref);
+            
+            
+            firebase.database().ref().child('messages').on('value', function(snapshot){
+
+            console.log("goes inside permission 1");
+            
              if (Notification.permission !== "granted")
                     Notification.requestPermission();
                   else {
 
+                    window.onblur = function() { onblur = true; console.log("onblur room 1"); };
+                    window.onfocus  = function() {onblur = false;  console.log("onfocus room 1"); };
+           
                     
-                    window.onblur = function() { onblur2 = true; console.log("onblur 2"); };
-                    window.onfocus  = function() {onblur2 = false;  console.log("onfocus 2"); };
+                    if(onblur2) {
+                        console.log("goes inside notification 1");
+                        var notification = new Notification('Anguchat', {
+                          icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+                          body: "New Message from room 1",
+                        });
+
+                        notification.onclick = function () {
+                          window.focus(); 
+                           $('#onebutton').trigger('click');
+                        };
+                    }
+                  }
+            
+
+         });
+         $scope.send = function(message) {
+            $scope.messages.$add({
+                message: $scope.messageText,
+                date: Date.now()
+            })
+         }
+})
+
+
+app.controller('ChatController2', function($scope, $firebaseArray) {
+
+            window.onblur = function() { onblur2 = true; console.log("onblur room 2"); };
+            window.onfocus  = function() {onblur2 = false;  console.log("onfocus room 2"); };
+
+		    var ref2 = firebase.database().ref().child('messages2').limitToLast(5);
+		    $scope.messages2 = $firebaseArray(ref2);
+            
+            
+		    firebase.database().ref().child('messages2').on('value', function(snapshot){
+
+            console.log("goes inside permission 2");
+            
+             if (Notification.permission !== "granted")
+                    Notification.requestPermission();
+                  else {
+
+                    window.onblur = function() { onblur2 = true; console.log("onblur room 2"); };
+                    window.onfocus  = function() {onblur2 = false;  console.log("onfocus room 2"); };
+           
+                    
                     if(onblur2) {
                         console.log("goes inside notification 2");
                         var notification = new Notification('Anguchat', {
@@ -74,12 +131,12 @@ app.controller('ChatController2', function($scope, $firebaseArray) {
             
 
          });
-              $scope.send = function(message) {
-                $scope.messages2.$add({
-                    message: $scope.messageText2,
-                    date: Date.now()
-                })
-             }
+         $scope.send2 = function(message) {
+            $scope.messages2.$add({
+                message: $scope.messageText2,
+                date: Date.now()
+            })
+         }
 })
 
 
