5d27fe9fbc4c6d0fd8f6bcaf2f2b88057be1138f
Pujan Parikh <Pujan Parikh>
2017-03-26 22:05:06 -0700

    faacebook auth added

--- public/js/app.js
+++ public/js/app.js
@@ -3,52 +3,9 @@ var app = angular.module('chatApp', ['ui.bootstrap', 'firebase']);
 var onblur;
 var onblur2;
 
-/*app.controller('ChatController', function($scope, $firebaseArray) {
 
-    window.onblur = function() { onblur = true; console.log("onblur room 1"); };
-    window.onfocus  = function() { onblur = false;  console.log("onfocus room 1"); };
-
-    var ref = firebase.database().ref().child('messages').limitToLast(5);
-    $scope.messages = $firebaseArray(ref);
-    
-    
-    firebase.database().ref().child('messages').on('value', function(snapshot){
-
-        console.log("goes inside update 1");
-
-        window.onblur = function() { onblur2 = true; console.log("onblur room 2"); };
-        window.onfocus  = function() {onblur2 = false;  console.log("onfocus room 2"); };
-    	
-          if (Notification.permission !== "granted")
-                    Notification.requestPermission();
-          else {
-                    
-               
-                    
-                if(onblur) {
-                    var notification = new Notification('Anguchat', {
-                          icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
-                           body: "New Message from room 1",
-                     });
-
-                    notification.onclick = function () {
-                       window.focus(); 
-                       $('#onebutton').trigger('click');
-                    };
-                }
-            }
-    });
-
-     $scope.send = function(message) {
-        $scope.messages.$add({
-            message: $scope.messageText,
-            date: Date.now()
-        })
-    }
-}) */
-
-
-app.controller('ChatController', function($scope, $firebaseArray) {
+app.controller('ChatController', function($scope, $firebaseArray, $firebaseAuth) {
+            var auth = $firebaseAuth();
 
             window.onblur = function() { onblur = true; console.log("onblur room 1"); };
             window.onfocus  = function() {onblur = false;  console.log("onfocus room 1"); };
@@ -90,6 +47,16 @@ app.controller('ChatController', function($scope, $firebaseArray) {
                 message: $scope.messageText,
                 date: Date.now()
             })
+         }
+
+         $scope.authfb = function(){
+           auth.$signInWithPopup("facebook").then(function(firebaseUser) {
+                
+                $scope.name = firebaseUser.user.displayName;
+                console.log("Signed in as:", firebaseUser);
+              }).catch(function(error) {
+                console.log("Authentication failed:", error);
+              });
          }
 })
 
