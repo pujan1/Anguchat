069a1be24dae770982efba2655d88497f2f68643
Pujan Parikh <Pujan Parikh>
2017-03-27 18:15:45 -0700

    port changed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public'))); 
 
@@ -10,4 +10,4 @@ app.get('/', function(req, res){
 	res.sendFile('/public/index.html');
 });
 app.listen(port);
-console.log('Listening on port 3001');
+console.log('Listening on port 3000');
